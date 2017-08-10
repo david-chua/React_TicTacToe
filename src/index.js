5f0f9ff796a8f4e2293f92e3a6ba969dcd2b14e4
david-chua <dp_chua@hotmail.com>
2017-08-10 01:14:30 -0400

    tictactoe

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+class Square extends React.Component {
+  render() {
+  return (
+    <button className="square">
+      {/* TODO */ }
+    </button>
+    );
+  }
+}
